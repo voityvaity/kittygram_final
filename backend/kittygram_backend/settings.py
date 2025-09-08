3d4a2debc423f33ad85cda7e4c61165ce1d2b47a
voityvaity <voityvaity@gmail.com>
2025-09-09 01:34:46 +0300

    4k off mfk

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', default='django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$')
 if not SECRET_KEY:
     raise RuntimeError("SECRET_KEY is not set in environment variables")
 
--- test_api.py
+++ test_api.py
@@ -2,19 +2,20 @@
 import requests
 import json
 
+
 def test_api():
     url = "http://localhost:8000/api/users/"
     data = {
         'username': 'newuser',
         'password': ''
     }
-    
+
     try:
         response = requests.post(url, data=data, timeout=15)
         print(f"Status Code: {response.status_code}")
         print(f"Headers: {response.headers}")
         print(f"Content: {response.text}")
-        
+
         # Проверяем, является ли ответ JSON
         try:
             json_data = response.json()
@@ -23,10 +24,11 @@ def test_api():
         except json.JSONDecodeError:
             print("Response is not valid JSON")
             return False
-            
+
     except requests.exceptions.RequestException as e:
         print(f"Request failed: {e}")
         return False
+
 
 if __name__ == "__main__":
     test_api()
